dfb9f155c15e64b869bd17fbccd35f986fb7cf20
Azizul Hakimi Mohd Yussuf Izzudin <ahmyi@users.noreply.github.com>
2018-07-15 02:03:30 +0800

    Refactoring on construct daemon
    Using isset compared to array_key_exists as isset will return false when the array value is set to null .

--- src/rpc/Daemon.php
+++ src/rpc/Daemon.php
@@ -5,7 +5,33 @@ namespace Stellite\Rpc;
 
 class Daemon extends Base{
 
+    public $url;
+    private $username;
+    private $password;
+  
+    public function __construct($url = 'http://127.0.0.1:20189', $username = null, $password = null){
+        if (is_array($host)) { // Parameters passed in as object/dictionary
+            $params = $url;
 
+            if (issset($params['url'])) {
+                $url = $params['url'];
+            } else {
+                $url = 'http://127.0.0.1:20189';
+            }
+
+            if (isset($params['user'])) {
+              $user = $params['user'];
+            }
+
+            if (isset($params['password'])) {
+              $password = $params['password'];
+            }
+        }
+
+        $this->username= $username;
+        $this->password = $password;
+        $this->url = $url;
+    }
 
 
 }
